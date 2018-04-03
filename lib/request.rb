d455867ee9c6a992449541cc5c0ab5a12641e89b
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-02 20:41:47 -0600

    Modify Request test and file, compile all iteration 1 requirements into initialize method of Request

--- lib/request.rb
+++ lib/request.rb
@@ -1,10 +1,27 @@
 
-
 class Request
   attr_reader     :request_lines
 
   def initialize(request_lines)
     @request_lines = request_lines
+    first_line = @request_lines[0].split(" ")
+    @verb = first_line[0]
+    @path = first_line[1]
+    @protocol = first_line[2]
+    get_host = @request_lines[1].split(": ")
+    @host = get_host[1]
+    get_port = @host.split(":")
+    @port = get_port[1]
+    @origin = get_host[1]
+    get_accept = @request_lines[4].split(": ")
+    @accept = get_accept[1]
+    print "Verb: #{@verb}\n"
+    print "Path: #{@path}\n"
+    print "Protocol: #{@protocol}\n"
+    print "Host: #{@host}\n"
+    print "Port: #{@port}\n"
+    print "Origin: #{@origin}\n"
+    print "Accept: #{@accept}\n"
   end
 
   def get_verb
--- lib/server.rb
+++ lib/server.rb
@@ -1,5 +1,5 @@
 require 'socket'
-require 'pry'
+require_relative 'request'
 
 class Server
 
@@ -13,18 +13,18 @@ class Server
       while line = client.gets and !line.chomp.empty?
         request_lines << line.chomp
       end
-      # request = Request.new(request_lines)
+      request = Request.new(request_lines)
       output = "Hello World! (#{count})"
       headers = ["http/1.1 200 ok",
                 "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
                 "server: ruby",
                 "content-type: text/html; charset=iso-8859-1",
                 "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-
+      client.puts request
       client.puts headers
       client.puts output
 
-      puts [headers, output].join("\n")
+      puts [headers, request, output].join("\n")
     end
 
       client.close
