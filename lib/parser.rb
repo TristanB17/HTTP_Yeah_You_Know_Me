63a0eba1f70f7e88ed7bd2830bba4392f3f3b94d
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-04 13:27:04 -0600

    Pass parser test(somewhat), desired results now return

--- lib/parser.rb
+++ lib/parser.rb
@@ -8,12 +8,11 @@ class Parser
     verb = request_lines[0].split(" ")[0]
     path = request_lines[0].split(" ")[1]
     protocol = request_lines[0].split(" ")[2]
-    host = request_lines[1].split(": ")[1]
+    host = request_lines[1].split(":")[1]
     port = request_lines[1].split(":")[2]
     origin = host
-    accept = request_lines[4]
-    binding.pry
-    "Verb: #{verb}\nPath: #{path}\nProtocol: #{protocol}\nHost: #{host}\nPort: #{port}\nOrigin: #{origin}\n#{accept}\n"
+    accept = request_lines[4].split(" ")[1]
+    "\nVerb: #{verb}\nPath: #{path}\nProtocol: #{protocol}\nHost:#{host}\nPort: #{port}\nOrigin:#{origin}\nAccepts: #{accept}\n"
   end
 
 
